108043ec5356b822f1647caada209b9306e838af
hassanwasswa <assanwasswa@gmail.com>
2021-02-25 14:53:03 +0100

    execrise 3 js

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,22 @@
 // Example
-window.alert("Hello World!");
+var firstNumber = parseInt(prompt('Insert a number of your choice', "1"), 10);
+var secondNumber =parseInt(prompt('Insert another number you want',"1"), 10);
+
+if (firstNumber > secondNumber ) {
+  window.alert('the greater number of '+ firstNumber + ' and ' + secondNumber + ' is '+ firstNumber)  
+} else if (secondNumber > firstNumber ){ 
+ window.alert('the greater number of '+ firstNumber + ' and ' + secondNumber + ' is '+ secondNumber)
+} else  {
+window.alert('the numbers are the same')
+}
+
+
+//Declare 2 variables, both numbers
+
+//Compare which number is greater
+
+//Log the output, e.g. "The greater number of 5 and 10 is 10."
+
+//Log the output, e.g. "The greater number of 5 and 10 is 5."
+
+//Add an output for the else statement, e.g. "The numbers are the same."
