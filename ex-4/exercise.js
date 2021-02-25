b4fde4a0bb66a3e9848382b5dc58d74ea0fb54c2
hassanwasswa <assanwasswa@gmail.com>
2021-02-25 15:23:14 +0100

    execrise4 js

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,12 @@
 // Example
-window.alert("Hello World!");
+var name = prompt('Enter your Name')
+alert('Ciao ' + name )
+var yourMoodToday = prompt('How is your day buddy')
+
+
+if ('bad'){
+    alert('Sorry buddy')
+} 
+else if ('good' ){
+    alert('Great!  ')
+}
